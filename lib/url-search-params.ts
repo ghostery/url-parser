86965d47906453b039151966b1a7bfe1762e92d0
Sam Macbeth <sam@cliqz.com>
2019-07-02 10:57:45 +0200

    Quick and dirty URLSearchParams implementation.

--- lib/url-search-params.ts
+++ lib/url-search-params.ts
@@ -24,7 +24,7 @@ export default class URLSearchParams implements IURLSearchParams {
     this.params = [];
   }
 
-  public * entries() {
+  public *entries() {
     for (let i = 0; i < this.params.length; i += 1) {
       const decoded: [string, string] = [
         optionalDecode(this.params[i][0]),
@@ -39,36 +39,68 @@ export default class URLSearchParams implements IURLSearchParams {
   }
 
   public delete(name: string) {
-    throw new Error('Method not implemented.');
+    this.params = this.params.filter(
+      ([key, _]) => optionalDecode(key) !== name,
+    );
   }
-  public forEach(callback: (value: string, name: string, searchParams: this) => void): void {
-    throw new Error('Method not implemented.');
+  public forEach(
+    callback: (value: string, name: string, searchParams: this) => void,
+  ): void {
+    this.params.forEach(([key, value]) => {
+      callback(optionalDecode(value), optionalDecode(key), this);
+    });
   }
   public get(name: string): string {
-    throw new Error('Method not implemented.');
+    const entry = this.params.find(([k, _]) => optionalDecode(k) === name);
+    if (entry) {
+      return optionalDecode(entry[1]);
+    }
+    return null;
   }
   public getAll(name: string): string[] {
-    throw new Error('Method not implemented.');
+    return this.params
+      .filter(([key, _]) => optionalDecode(key) === name)
+      .map(kv => kv[1]);
   }
   public has(name: string): boolean {
-    throw new Error('Method not implemented.');
+    return this.get(name) !== null;
   }
-  public keys(): IterableIterator<string> {
-    throw new Error('Method not implemented.');
+  public *keys(): IterableIterator<string> {
+    for (let i = 0; i < this.params.length; i += 1) {
+      yield optionalDecode(this.params[i][0]);
+    }
   }
+
+  /**
+   * The set() method of the URLSearchParams interface sets the value associated with a given
+   * search parameter to the given value. If there were several matching values, this method
+   * deletes the others. If the search parameter doesn't exist, this method creates it.
+   * @param name
+   * @param value
+   */
   public set(name: string, value: string): void {
-    throw new Error('Method not implemented.');
+    const firstIndex = this.params.findIndex(
+      ([k, _]) => optionalDecode(k) === name,
+    );
+    if (firstIndex === -1) {
+      this.append(name, value);
+      return;
+    }
+    this.delete(name);
+    this.params.splice(firstIndex, 0, [name, value]);
   }
   public sort(): void {
-    throw new Error('Method not implemented.');
+    this.params = this.params.sort((a, b) => a[0].localeCompare(b[0]));
   }
   public toString(): string {
-    throw new Error('Method not implemented.');
+    return this.params.map(([k, v]) => `${k}=${v}`).join('&');
   }
-  public values(): IterableIterator<string> {
-    throw new Error('Method not implemented.');
+  public *values(): IterableIterator<string> {
+    for (let i = 0; i < this.params.length; i += 1) {
+      yield optionalDecode(this.params[i][1]);
+    }
   }
   public [Symbol.iterator](): IterableIterator<[string, string]> {
-    throw new Error('Method not implemented.');
+    return this.entries();
   }
 }
