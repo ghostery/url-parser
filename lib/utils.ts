b47c8e236b7fed3705a0fd24ca0e51f979d47c71
Rémi Berson <remi@cliqz.com>
2019-07-05 07:46:08 +0200

    fix implicity any type in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,7 @@ import URL from './url';
  * Checks if this URL's hostname is non-ascii, and if so returns a new URL with the hostname
  * punycoded. Otherwise returns itself.
  */
-export function getPunycodeEncoded(toASCII: (s) => string, url: URL) {
+export function getPunycodeEncoded(toASCII: (s: string) => string, url: URL) {
   const punycodedHost = toASCII(url.hostname);
   if (punycodedHost !== url.hostname) {
     return new URL(`${url.protocol}${url.slashes}${punycodedHost}${url.pathname}${url.search}${url.hash}`);
