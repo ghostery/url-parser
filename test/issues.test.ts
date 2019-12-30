9f3a08c7c6c208d16e3d0df37b4925018ce45abb
Sam Macbeth <sam@cliqz.com>
2019-12-30 14:00:32 +0100

    Add testcase for setting querystring. Fixes #22

---
+++ test/issues.test.ts
@@ -0,0 +1,9 @@
+import { URL } from '../url-parser';
+
+describe('Github', () => {
+  it('Issue 22: Unable to set querystring', () => {
+    const url = new URL('https://github.com');
+    url.searchParams.set('key', 'value');
+    expect(url.href).toEqual('https://github.com/?key=value');
+  });
+});
